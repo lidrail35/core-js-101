45cccdae6a6912b4c8005aa3ba27673d22680fe9
lidrail35 <lidrail35@mail.ru>
2023-04-17 21:54:58 +0300

    feat: add solution 02-numbers-tasks-isPrime

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -207,8 +207,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n===2) return true;
+  for (let i = 2; i <= n/2+1; i++) {
+    if ( n/i === Math.round(n / i) ) return false;
+  }
+  return true;
 }
 
 /**
