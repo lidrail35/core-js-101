601d826e3bf5b0f6e1212f857b9c1cade0dee9df
lidrail35 <lidrail35@mail.ru>
2023-04-17 23:27:13 +0300

    feat: add solution 03-arrays-tasks-findElement

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -51,8 +51,11 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+
+  console.log('-------------------', value1);
+  console.log('-------------------', value2);
+  return (value1 + value2) / 2;
 }
 
 /**
