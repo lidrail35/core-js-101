f37db9319949161f2c0b687c3acff00bdfa69ebb
lidrail35 <lidrail35@mail.ru>
2023-04-21 21:49:50 +0300

    feat: add solution 03-arrays-tasks-propagateItemsByPositionIndex

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -271,8 +271,16 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return  arr.map( function(x, i) {
+            let arrB = [];
+            
+            arrB.length = i;
+            arrB.fill(x, 0, i);
+            return [x, ...arrB];
+          }).flat();
+//  [...arr.map( (x, i) => (' '+x).repeat(i+1)).map(x => x.split(' '))].map(x => x.filter(x=> x!= '')).flat();
+  //throw new Error('Not implemented');
 }
 
 
