198c8113488cf016966ef3ba922b8396d43641ea
lidrail35 <lidrail35@mail.ru>
2023-06-15 15:03:01 +0300

    feat: add solution 04-date-tasks-angleBetweenClockHands

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -102,8 +102,13 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = date.getUTCHours();
+  const minute = date.getUTCMinutes();
+  let angle = Math.abs(0.5 * (60 * hour - 11 * minute));
+  angle %= 360;
+  if (angle > 180) angle = Math.abs(360 - angle);
+  return ((angle * Math.PI) / 180);
 }
 
 
